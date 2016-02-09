9a2202ed0a1eb4720b96fa48520dea031336d3b5
Zbigniew Tomczak <zbigniew.tomczak@pl.abb.com>
2016-02-09 13:41:24 +0100

    refactor renaming

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -3,40 +3,41 @@ public class BinarySearch implements Search {
 	private final int endIndex;
 	private final int[] A;
 	public static final int NOT_FOUND_VALUE = -1;
-	private int middleIndex;
+	private int firstIndexOfSecondHalf;
 
 	public BinarySearch(int[] A) {
-		this(A, 0, A.length - 1);
+		this(A, 0, arrayLastIndex(A));
 	}
 
 	private BinarySearch(int[] A, int beginIndex, int endIndex) {
 		this.beginIndex = beginIndex;
 		this.endIndex = endIndex;
-		this.middleIndex = calculateMiddle(endIndex);
+		this.firstIndexOfSecondHalf = calculateBeginningOfSecondHalf(endIndex);
 		this.A = A;
 	}
 
-	private int calculateMiddle(int endIndex) {
+	private int calculateBeginningOfSecondHalf(int endIndex) {
 		return 1; //TODO simplification
 	}
 
 	@Override
 	public int find(int n) {
-		System.out.println(beginIndex +", "+ endIndex +", "+ middleIndex);
+		System.out.println(beginIndex +", "+ endIndex +", "+ firstIndexOfSecondHalf);
 		if (isSizeOne()) {
 			return firstMatches(n) ? beginIndex : NOT_FOUND_VALUE;
 		}
 		if (inFirstHalf(n)) {
-			return new BinarySearch(A, beginIndex, middleIndex - 1).find(n);
+			int lastIndexOfFirstHalf = firstIndexOfSecondHalf - 1;
+			return new BinarySearch(A, beginIndex, lastIndexOfFirstHalf).find(n);
 		}
 		if (inSecondHalf(n)) {
-			return new BinarySearch(A, middleIndex, endIndex).find(n);
+			return new BinarySearch(A, firstIndexOfSecondHalf, endIndex).find(n);
 		}
 		return NOT_FOUND_VALUE;
 	}
 
 	private boolean inFirstHalf(int n) {
-		return n < A[middleIndex];
+		return n < A[firstIndexOfSecondHalf];
 	}
 
 	private boolean inSecondHalf(int n) {
@@ -49,5 +50,9 @@ public class BinarySearch implements Search {
 
 	private boolean isSizeOne() {
 		return beginIndex == endIndex;
+	}
+
+	private static int arrayLastIndex(int[] A) {
+		return A.length - 1;
 	}
 }
